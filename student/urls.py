02a9a00895a7797c5b814c6c44bb7bfc63b682ef
Jiaxiang <fujiaxiang1995@gmail.com>
2015-09-25 22:50:07 +0800

    Just testing for fun haha

--- student/urls.py
+++ student/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from student import views
+
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^signup', views.signup, name="signup"),
