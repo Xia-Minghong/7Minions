c3585bf0e5e4b7fb3de843bd02eb04ceb965d8f4
WhiteHat <mxia001@e.ntu.edu.sg>
2015-09-25 23:09:06 +0800

    change

--- student/urls.py
+++ student/urls.py
@@ -3,6 +3,6 @@ from student import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^signup', views.signup, name="signup"),
+    url(r'^signup/(?P<matric_no>.*)/(?P<name>.*)/', views.signup, name="signup"),
     url(r'^login', views.login, name="login"),
 ]
--- student/views.py
+++ student/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from student.models import *
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("1")
+
+def signup(request, matric_no, name):
+    return HttpResponse(matric_no)
+
+def login(request):
+    return HttpResponse(0)
