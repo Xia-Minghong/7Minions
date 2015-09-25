aa5c6ae1ee7c0ec90a35307560dd0cddf9e355bb
WhiteHat <mxia001@e.ntu.edu.sg>
2015-09-26 00:27:05 +0800

    change

--- student/views.py
+++ student/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from student.models import *
 from django.http import HttpResponse
+from django.core import serializers
 import json
 
 # Create your views here.
@@ -10,7 +11,7 @@ def index(request):
 def signup(request, matric_no, name):
     new_student = Student(matric_no=matric_no, name=name)
     new_student.save()
-    return HttpResponse(json.dumps(new_student), content_type="application/json")
+    return HttpResponse(json.dumps(serializers.serialize("json", [new_student])), content_type="application/json")
 
 def login(request):
     return HttpResponse(0)
