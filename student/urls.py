434d937d5d4f9e2ad8a472cd9dd485d0243cc8b2
Jiaxiang <fujiaxiang1995@gmail.com>
2015-09-25 22:48:24 +0800

    Just testing for fun haha

--- student/urls.py
+++ student/urls.py
@@ -3,4 +3,5 @@ from student import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^signup', views.signup, name="signup"),
+    url(r'^login', views.login, name="login"),
 ]
