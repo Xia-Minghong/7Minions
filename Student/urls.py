6a285c9b6a0bb25414e70b15dedd4be06f92304f
WhiteHat <mxia001@e.ntu.edu.sg>
2015-09-25 22:41:00 +0800

    change

--- Student/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from student import views
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^signup/', views.signup, name='signup'),
-    url(r'^home', views.home, name='home'),
-)
