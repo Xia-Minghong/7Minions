0ca99a6b2c108550b3dae00d7626e088eaf749f8
WhiteHat <mxia001@e.ntu.edu.sg>
2015-09-25 22:51:00 +0800

    change

--- student/models.py
+++ student/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    matric_no = models.Charfield(max_length=9)
+    matric_no = models.CharField(max_length=9)
     name = models.CharField(max_length=40)
