f2abc7d06ec1b9c50c51ee00afab68b883d2cc5b
Jiaxiang <fujiaxiang1995@gmail.com>
2015-09-26 12:15:53 +0800

    added in: list display Friendship

--- student/admin.py
+++ student/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Friendship
 
 # Register your models here.
 
+class FriendshipInline(admin.StackedInline):
+    model = Friendship
+    fk_name = 'from_student'
+
+
 class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'matric_no')
     fieldsets = [
         ('Matric Number',   {'fields': ['matric_no']}),
         ('Name',            {'fields': ['name'], 'classes': ['collapse']}),
     ]
+    inlines = [FriendshipInline]
+
 
 admin.site.register(Student, StudentAdmin)
--- student/models.py
+++ student/models.py
@@ -4,3 +4,15 @@ from django.db import models
 class Student(models.Model):
     matric_no = models.CharField(max_length=9)
     name = models.CharField(max_length=40)
+    friends = models.ManyToManyField('self', through='Friendship',
+                                           symmetrical=False,
+                                           related_name='related_to')
+
+    # def __unicode__(self):
+    #    return 'Student: ' + self.name
+
+class Friendship(models.Model):
+    from_student = models.ForeignKey(Student, related_name='from_student')
+    to_student = models.ForeignKey(Student, related_name='to_student')
+    # status = models.IntegerField(choices=RELATIONSHIP_STATUSES)
+
