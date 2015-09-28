56b62ab1cd3301b66e5b6ed774ab4a2d2a088f0a
Jiaxiang <fujiaxiang1995@gmail.com>
2015-09-28 21:26:00 +0800

    added in: new apps

--- event/models.py
+++ event/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from organizer.models import Organizer
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=20)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+    #tag =
+    organizer = models.ForeignKey(Organizer)
+    likes = models.IntegerField()
