6feb77b2c2c5209a7619ecfd06df499511cb0692
WhiteHat <mxia001@e.ntu.edu.sg>
2015-09-28 21:52:24 +0800

    model

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from event.models import Event
+from student.models import Student
 # Create your models here.
+
+class Registration(models.Model):
+
+    student = models.ForeignKey(Student)
+    event = models.ForeignKey(Event)
+
+    class Meta:
+        unique_together = (("student", "event"),)
--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from event.models import Event
 # Create your models here.
+
+class Registration(models.Model):
+
+    tag = models.CharField(max_length=40)
+    event = models.ForeignKey(Event)
+
+    class Meta:
+        unique_together = (("student", "event"),)
