fc83e9d75bbc42979d467a24a7769c8b3449b34b
WhiteHat <mxia001@e.ntu.edu.sg>
2015-09-28 21:40:09 +0800

    url

--- student/models.py
+++ student/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
-    matric_no = models.CharField(max_length=9)
-    name = models.CharField(max_length=40)
-    email = models.CharField(max_length=20)
+
+    user = models.OneToOneField(User)
+
+    department = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=50)
+
+    matric_no = models.CharField(max_length=9, unique=True)
+
+    email = models.CharField(max_length=100, unique=True)
     #preference = models.
-    password = models.CharField(max_length=20)
 
     friends = models.ManyToManyField('self', through='Friendship', symmetrical=False,
                                            related_name='related_to'
