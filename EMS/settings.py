e3deb7b4735af404caf251feca5d6d69def3aef3
Jiaxiang <fujiaxiang1995@gmail.com>
2015-09-28 21:32:43 +0800

    added in: new apps

--- EMS/settings.py
+++ EMS/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'event'
+    'feedback'
+    'organizer'
+    'registration'
     'student'
+    'tag'
 )
 
 MIDDLEWARE_CLASSES = (
