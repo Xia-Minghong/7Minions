789a2dbb906c787206e0b7c958ceb55d43e5c9d0
Jiaxiang <fujiaxiang1995@gmail.com>
2015-10-10 12:39:46 +0800

    small changes

--- feedback/admin.py
+++ feedback/admin.py
@@ -4,7 +4,7 @@ from .models import Feedback
 
 
 class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('content', 'rating')
+    list_display = ('id', 'content', 'rating')
 
 
 # Register your models here.
