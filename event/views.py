577faa8c9bb656ded7b719d7c08fb0d102174031
Jiaxiang <fujiaxiang1995@gmail.com>
2015-10-10 23:34:44 +0800

    finished register_event

--- event/views.py
+++ event/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, permissions
 from .models import Event
 from .serializers import EventSerializer
+from rest_framework.decorators import list_route, detail_route
+from organizer.models import Organizer
+from rest_framework.response import Response
+
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 # Create your views here.
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    
+    @detail_route(methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def like(self, request, pk = None):
+        event = get_object_or_404(Event.objects.all(), pk = pk)
+        event.likes += 1
+        event.save()
+        return Response(event.likes)
 
 
-
-
+    # POST http://127.0.0.1:8000/events/
+    def create(self, request, *args, **kwargs):
+        serialized = EventSerializer(data=request.data)
+        organizer= Organizer.objects.get(id=serialized.initial_data["organizer"],)
+        event = Event(
+                name=serialized.initial_data["name"],
+                start_time=serialized.initial_data["start_time"],
+                end_time=serialized.initial_data["end_time"],
+                description=serialized.initial_data["description"],
+                organizer = organizer,
+                likes=serialized.initial_data["likes"],
+                img_url=serialized.initial_data["img_url"],
+            )
+        event.save()
+        return Response()#serialized.data, content_type="application/json")
 
 # View all events
 # GET:  http://127.0.0.1:8000/events/
