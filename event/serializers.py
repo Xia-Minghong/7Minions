611c858e307cae005c6b37ce14ea14779074abed
Jiaxiang <fujiaxiang1995@gmail.com>
2015-10-10 18:43:09 +0800

    small changes

--- event/serializers.py
+++ event/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-
+from tag.serializers import TagSerializer
 from .models import Event
 
 class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ('id', 'name', 'start_time', 'end_time', 'location', 'description', 'organizer', 'likes', 'img_url')
-
+        fields = ('id', 'name', 'start_time', 'end_time', 'location', 'description', 'likes', 'img_url', 'organizer', 'tag_set')
+        depth = 1
