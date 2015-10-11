ada724f88136db1f9c41b756741001606e9f350d
Harvey <harveyleo.lhh@gmail.com>
2015-10-12 01:07:22 +0800

    Change "Django administration"

--- event/admin.py
+++ event/admin.py
@@ -5,6 +5,11 @@ from .models import Event
 class EventAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
 
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return Event.objects.all()
+        return Event.objects.filter(organizer = 1)
+
 
 # Register your models here.
 admin.site.register(Event, EventAdmin)
--- feedback/admin.py
+++ feedback/admin.py
@@ -3,9 +3,18 @@ from django.contrib import admin
 from .models import Feedback
 
 
+
+
+
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('event', 'content', 'rating')
+    list_filter = ('event',)
 
+
+    # def get_queryset(self, request):
+    #     if request.user.is_superuser:
+    #         return Feedback.objects.all()
+    #     return Feedback.objects.filter(event = 1)
 
 # Register your models here.
 admin.site.register(Feedback, FeedbackAdmin)
