70efe727200c5ff279fefb4305bed6cea1553c85
Harvey <harveyleo.lhh@gmail.com>
2015-10-12 09:58:01 +0800

    Change "Django administration" tag

--- tag/admin.py
+++ tag/admin.py
@@ -4,6 +4,12 @@ from .models import Tag
 
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id','tag', 'event')
+    list_filter = ('tag',)
+
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return Tag.objects.all()
+        return Tag.objects.filter(event__organizer__id = 1)
 
 
 # Register your models here.
